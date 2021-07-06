5db43a3597bc0f220720325b65c592a4f5f4c989
Sirus97 <86083633+Sirus97@users.noreply.github.com>
2021-07-05 20:01:00 -0400

    Give every channel a separate Leaves instance

--- src/main/handlers/leaves.py
+++ src/main/handlers/leaves.py
@@ -11,7 +11,6 @@ MESSAGE_COUNT = 'message_count'
 NUM_WAIT_MESSAGES = 'number_of_messages_to_wait_for'
 LEAVES_SENT = 'leaves_sent'
 TIMES_REPEAT = 'times_to_repeat'
-CHANNEL = 'channel'
 LAST_LEAVES = 'last_leaves'
 
 class Leaves:
@@ -26,50 +25,48 @@ class Leaves:
         self.leaves_sources = ['🍃', '🌳']
         self.cooldown = 30
 
-        self.server_vars = {}
+        self.channel_vars = {}
 
     async def execute(self, message):
-        if not message.guild.id in self.server_vars:
-            self.server_vars[message.guild.id] = {}
-            self.server_vars[message.guild.id][ONGOING] = False
-            self.server_vars[message.guild.id][MESSAGE_COUNT] = 0
-            self.server_vars[message.guild.id][NUM_WAIT_MESSAGES] = 5
-            self.server_vars[message.guild.id][LEAVES_SENT] = 0
-            self.server_vars[message.guild.id][TIMES_REPEAT] = 5
-            self.server_vars[message.guild.id][CHANNEL] = message.channel
-            self.server_vars[message.guild.id][LAST_LEAVES] = datetime.min
+        if not message.channel.id in self.channel_vars:
+            self.channel_vars[message.channel.id] = {}
+            self.channel_vars[message.channel.id][ONGOING] = False
+            self.channel_vars[message.channel.id][MESSAGE_COUNT] = 0
+            self.channel_vars[message.channel.id][NUM_WAIT_MESSAGES] = 5
+            self.channel_vars[message.channel.id][LEAVES_SENT] = 0
+            self.channel_vars[message.channel.id][TIMES_REPEAT] = 5
+            self.channel_vars[message.channel.id][LAST_LEAVES] = datetime.min
 
-        if self.server_vars[message.guild.id][ONGOING] and message.channel == self.server_vars[message.guild.id][CHANNEL]:
+        if self.channel_vars[message.channel.id][ONGOING]:
             if self.leaves_emoji in message.content:
-                self.server_vars[message.guild.id][MESSAGE_COUNT] = 0
+                self.channel_vars[message.channel.id][MESSAGE_COUNT] = 0
             else:
-                self.server_vars[message.guild.id][MESSAGE_COUNT] += 1
+                self.channel_vars[message.channel.id][MESSAGE_COUNT] += 1
 
-                if self.server_vars[message.guild.id][MESSAGE_COUNT] == self.server_vars[message.guild.id][NUM_WAIT_MESSAGES]:
-                    if self.server_vars[message.guild.id][LEAVES_SENT] < self.server_vars[message.guild.id][TIMES_REPEAT]:
-                        await self.server_vars[message.guild.id][CHANNEL].send(content=self.leaves_alias)
-                        self.server_vars[message.guild.id][LEAVES_SENT] += 1
-                        self.server_vars[message.guild.id][NUM_WAIT_MESSAGES] = random.choice(range(3,6))
-                    elif self.server_vars[message.guild.id][LEAVES_SENT] == self.server_vars[message.guild.id][TIMES_REPEAT]:
-                        await self.server_vars[message.guild.id][CHANNEL].send(content=random.choices((self.leaves_destroyer, random.choice(self.config['GIFs'])), weights=[90, 10])[0])
-                        self.server_vars[message.guild.id][ONGOING] = False
-                    self.server_vars[message.guild.id][MESSAGE_COUNT] = 0
+                if self.channel_vars[message.channel.id][MESSAGE_COUNT] == self.channel_vars[message.channel.id][NUM_WAIT_MESSAGES]:
+                    if self.channel_vars[message.channel.id][LEAVES_SENT] < self.channel_vars[message.channel.id][TIMES_REPEAT]:
+                        await message.channel.send(content=self.leaves_alias)
+                        self.channel_vars[message.channel.id][LEAVES_SENT] += 1
+                        self.channel_vars[message.channel.id][NUM_WAIT_MESSAGES] = random.choice(range(3,6))
+                    elif self.channel_vars[message.channel.id][LEAVES_SENT] == self.channel_vars[message.channel.id][TIMES_REPEAT]:
+                        await message.channel.send(content=random.choices((self.leaves_destroyer, random.choice(self.config['GIFs'])), weights=[90, 10])[0])
+                        self.channel_vars[message.channel.id][ONGOING] = False
+                    self.channel_vars[message.channel.id][MESSAGE_COUNT] = 0
 
         if any(leaves_source in message.content for leaves_source in self.leaves_sources):
             print(f'{datetime.now().isoformat()} Leaves triggered!')
             print(message.author, '-', message.content, message.jump_url)
 
-            if (datetime.now() - self.server_vars[message.guild.id][LAST_LEAVES]) < timedelta(minutes=self.cooldown):
+            if (datetime.now() - self.channel_vars[message.channel.id][LAST_LEAVES]) < timedelta(minutes=self.cooldown):
                 print(f'{datetime.now().isoformat()} Leaves is on cooldown for '
-                    f'{self.cooldown - ((datetime.now() - self.server_vars[message.guild.id][LAST_LEAVES]).seconds // 60)} minutes')
-            elif self.server_vars[message.guild.id][ONGOING]:
+                    f'{self.cooldown - ((datetime.now() - self.channel_vars[message.channel.id][LAST_LEAVES]).seconds // 60)} minutes')
+            elif self.channel_vars[message.channel.id][ONGOING]:
                 print('Another leaves is currently still going!')
             elif not message.channel.permissions_for(message.guild.get_member(self.client.user.id)).send_messages:
                 print('No permission to send messages in this channel!')
             else:
                 print(f'{datetime.now().isoformat()} Throwing leaves!')
-                self.server_vars[message.guild.id][CHANNEL] = message.channel
-                self.server_vars[message.guild.id][TIMES_REPEAT] = random.choices((random.choice(range(1, 10)), 30), weights=[95, 5])[0]
-                self.server_vars[message.guild.id][NUM_WAIT_MESSAGES] = random.choice(range(3,6))
-                self.server_vars[message.guild.id][ONGOING] = True
-                self.server_vars[message.guild.id][LAST_LEAVES] = datetime.now()
+                self.channel_vars[message.channel.id][TIMES_REPEAT] = random.choices((random.choice(range(1, 10)), 30), weights=[95, 5])[0]
+                self.channel_vars[message.channel.id][NUM_WAIT_MESSAGES] = random.choice(range(3,6))
+                self.channel_vars[message.channel.id][ONGOING] = True
+                self.channel_vars[message.channel.id][LAST_LEAVES] = datetime.now()
